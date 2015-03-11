b349aa786f2a89ee54b34a40dd14087c8708510b
Hudi89 <hudi1989@gmail.com>
2015-03-11 09:37:13 +0100

    Create tetelek.cpp

---
+++ 04-matrix2/feladat1/tetelek.cpp
@@ -0,0 +1,86 @@
+#include "tetelek.h"
+
+using namespace std;
+
+istream &operator >> (istream &in , vector<int> &t)
+{
+    int a;
+    int n;
+    in >> n;
+    if (n<0){throw NEGATIV_COUNT;}
+    else if (n==0){throw EMPTY_MATRIX;}
+    t.resize(n);
+    for (int i=0; i<t.size(); ++i)
+    {
+        in >> a;
+        if (a>10){throw TOO_MANY_SCORES;}
+        t[i]=a;
+    }
+    return in;
+}
+
+istream &operator >> (istream &in , vector<vector<int> > &t)
+{
+    vector<int> a;
+    int n;
+    in >> n;
+    if (n<0){throw NEGATIV_TOURNAMENT;}
+    else if (n==0){throw NULL_MATCH;}
+    t.resize(n);
+    for (int i=0; i<t.size(); ++i)
+    {
+        in >> a;
+        t[i]=a;
+    }
+    return in;
+}
+
+int sorOsszeg(vector<int> &v)
+{
+    int sum=0;
+    //null(sum);
+    for(int i = 0 ; i < v.size() ; ++i)
+    {
+        sum = sum + v[i];
+    }
+    return sum;
+}
+
+int sorMennyiseg (vector<int> &v)
+{
+    int db=0;
+    for(int i = 0 ; i < v.size() ; ++i)
+    {
+        if(v[i]>3){
+            db += 1;
+        }
+    }
+    return db;
+}
+
+int feltMaxKer(vector<vector<int> > &v)
+{
+    int M = 0;
+    int ind = 0;
+    bool l = false;
+    for(int i=0;i<v.size();++i){
+        if(sorMennyiseg(v[i])>=1){
+                if(l){
+                    if(M < (float)sorOsszeg(v[i])/(float)v[i].size()){
+                        M = (float)sorOsszeg(v[i])/(float)v[i].size();
+                        ind = i;
+                    }
+                }else{
+                    M = (float)sorOsszeg(v[i])/(float)v[i].size();
+                    ind = i;
+                    l=true;
+                }
+        }
+    }
+    if(!l){
+      throw EVERYBODY_IS_DEAD;
+    }
+    else{
+      return ind;
+    }
+}
