2586cc5de458250492f76baadf0a15061ec7c00a
Mikecz Márk László <locutus@inf.elte.hu>
2015-04-30 16:45:13 +0200

    Update main.cpp
    Kicsit kijavítottam :D Betettem azt a feladatot, hogy az emberek fizuja meg neve van tárolva :D

--- 11-merge/main.cpp
+++ 11-merge/main.cpp
@@ -53,6 +53,31 @@ public:
     }
 };
 
+class Adatok{
+    string name;
+    int kereset;
+public:
+    string getName() {
+        return name;
+    }
+
+    int getKereset() {
+        return kereset;
+    }
+    friend istream& operator>>(istream& in, Adatok &adatok);
+    friend ostream& operator<<(ostream& out,const Adatok &adatok);
+};
+
+istream& operator>>(istream& in, Adatok &adatok) {
+    in >> adatok.name >> adatok.kereset;
+    return in;
+}
+
+ostream& operator<<(ostream& out,const Adatok &adatok) {
+    out << adatok.name <<" "<< adatok.kereset<<endl;
+    return out;
+}
+
 int main()
 {
     FileEnumerator<ppl> si1("in1.txt");
@@ -61,8 +86,19 @@ int main()
     si1.init();
     si2.init();
 
-    while(!(si1.isEnd() || si2.isEnd())){
-
+    while (!(si1.isEnd() || si2.isEnd())) {
+        if (si1.current().getName() == si2.current().getName()) {
+            so.write(si1.current());
+            si1.next();
+            si2.next();
+        } else {
+            if (si1.current().getName() < si2.current().getName()) {
+                si1.next();
+            }
+            else {
+                si2.next();
+            }
+        }
     }
     return 0;
 }
