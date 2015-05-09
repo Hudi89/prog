db96f547e08f8b7663c801ecb955c031e57a9fee
Hudi89 <hudi1989@gmail.com>
2015-05-09 22:06:34 +0200

    Update main.cpp

--- 11-merge/main.cpp
+++ 11-merge/main.cpp
@@ -3,19 +3,31 @@
 
 using namespace std;
 
+
+//Megfogalmazunk egy absztrakt osztályt, hogy milyen függvényeket várunk el egy felsoroló osztálytól
 template<typename T>
 class Enumerator{
 public:
+    //Visszaadja azt az elemet amin állunk
     virtual T current() = 0;
+    
+    //Következő elemre lép a szekvencia
     virtual void next() = 0;
+    
+    //Visszadja, hogy vége van-e az szekvenciának
     virtual bool isEnd() = 0;
+    
+    //A felsorolót inicializálja (Valójában elhagyható lenne)
     virtual void init() = 0;
 };
 
+
+//Egy olyan osztály ami a konstruktorában egy fájl nevet vár majd a >> operátorral felsorolja a T típusú elemeket
 template<typename T>
 class FileEnumerator : public Enumerator<T>{
 private:
     ifstream f;
+    //Ebben tároljuk mindig az aktuális elemet, amin áll a felsorolónk
     T actualItem;
 public:
     FileEnumerator(string filename){
@@ -23,11 +35,16 @@ public:
     }
 
     void init(){
+        //A fájl elejére ugrunk. Ezzel a funkciót is megvalósítjuk, hogy újra 
+        //inicializálás esetében a szekvencia előről kezdődik (Ezt nem várjuk el amúgy)
         f.seekg(0);
+        
+        //Beolvassuk az első elemet
         next();
     }
     bool isEnd(){
-        return f.eof();
+        //Akkor állunk le, ha az olvasás sikertelen volt
+        return f.fail();
     }
     void next(){
         f >> actualItem;
@@ -86,19 +103,19 @@ int main()
     si1.init();
     si2.init();
 
+    //Metszet
     while (!(si1.isEnd() || si2.isEnd())) {
         if (si1.current().getName() == si2.current().getName()) {
             so.write(si1.current());
             si1.next();
             si2.next();
-        } else {
-            if (si1.current().getName() < si2.current().getName()) {
-                si1.next();
-            }
-            else {
-                si2.next();
-            }
+        } else if (si1.current().getName() < si2.current().getName()) {
+            si1.next();
         }
+        else {
+            si2.next();
+        }
+
     }
     return 0;
 }
