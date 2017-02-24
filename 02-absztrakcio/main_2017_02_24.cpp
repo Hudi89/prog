3a8e8bbf414ba75ece80587a0b9b0ff1872408b0
Mikecz Márk László <mikecz.mark.laszlo@gmail.com>
2017-02-24 13:51:39 +0100

    Update main_2017_02_24.cpp

--- 02-absztrakcio/main_2017_02_24.cpp
+++ 02-absztrakcio/main_2017_02_24.cpp
@@ -14,6 +14,18 @@ istream& operator>>(istream& in,vector<int> &a) {
     return in;
 }
 
+istream& operator>>(istream& in,vector<vector<int> > &a) {
+    int size;
+    in >> size;
+    vector<int> temp;
+    for(int i = 0; i < size; ++i) {
+        temp.clear();
+        in >> temp;
+        a.push_back(temp);
+    }
+    return in;
+}
+
 int& szamol(const vector<int> &v) {
     int c = 0;
     for(int i = 0; i < v.size(); ++i) {
@@ -33,15 +45,61 @@ int& osszeg(const vector<int> &v)
     return s;
 }
 
+int& max_vector(const vector<int> &a) {
+    int error = -1;
+    if(a.size() == 0) {
+        return error;
+    }
+    int max_h = a[0];
+    for(int i = 1; i < a.size(); ++i) {
+        int temp = a[i];
+        if(max_h < temp) {
+            max_h = temp;
+        }
+    }
+    return max_h;
+}
+
+
+int& max_osszeg_matrix(const vector<vector<int> > &a) {
+    int error = -1;
+    if(a.size() == 0) {
+        return error;
+    }
+    int max_h = osszeg(a[0]);
+    for(int i = 1; i < a.size(); ++i) {
+        int temp = osszeg(a[i]);
+        if(max_h < temp) {
+            max_h = temp;
+        }
+    }
+    return max_h;
+}
+
 int main()
 {
-    vector<int> v;
+    //==== VECTOR =====
+    /*vector<int> v;
     cin >> v;
     cout << "Összeg: " << osszeg(v) << endl;
 
     cout << "DEBUG" << endl;
     for( int i = 0; i < v.size(); ++i ) {
         cout << v.at(i) << endl;
+    }*/
+    //=== MÁTRIX ====
+    vector<vector<int> > matrix;
+    cin >> matrix;
+
+    cout << "Maximum: " << max_Osszeg_matrix(matrix) << endl;
+
+    cout << "DEBUG" << endl;
+    for(int i = 0; i < matrix.size(); ++i) {
+        for(int j = 0; j < matrix[i].size(); ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
     }
+
     return 0;
 }
